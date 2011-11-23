d372b7ed31d26ee53ff44121eb1cd4344089a48f
Shu Shen <shu.shen@gmail.com>
2011-11-22 21:45:12 -0800

    [mq]: Fix for issue 12 - occassional failure when parsing prevents file opening

--- src/view/CtagsManager.py
+++ src/view/CtagsManager.py
@@ -1,11 +1,20 @@
 import subprocess
 
+def _eintr_retry_call(func, *args):
+	while True:
+		try:
+			return func(*args)
+	except OSError, e:
+		if e.errno == errno.EINTR:
+			continue
+		raise
+
 def ct_query(filename):
 	cmd = 'ctags -n -u --fields=+K -f -'
 	args = cmd.split()
 	args.append(filename)
 	proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-	(out_data, err_data) = proc.communicate()
+	(out_data, err_data) = _eintr_retry_call(proc.communicate)
 	out_data = out_data.split('\n')
 	res = []
 	for line in out_data:
