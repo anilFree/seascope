1339c153dff9bae8b9cada021a5138233c81785d
Shu Shen <shu.shen@gmail.com>
2011-11-24 10:20:46 -0800

    Add support for changing folder in the tree view

--- src/view/FileView.py
+++ src/view/FileView.py
@@ -34,21 +34,44 @@ class FileTree(QTabWidget):
 
 		# Tree view
 		self.treew = QWidget()
-		self.tl = QLabel()
 		self.tmodel = QFileSystemModel()
 		self.tmodel.setRootPath(QDir.rootPath())
 		self.tview = QTreeView()
 		self.tview.setHeaderHidden(True)
 
+		self.ted = QLineEdit()
+		self.completer = QCompleter()
+		self.completer.setCompletionMode(QCompleter.PopupCompletion)
+		self.ted.setCompleter(self.completer)
+		self.tdbtn = QPushButton()
+		self.tdbtn.setIcon(QIcon.fromTheme("document-open", QIcon("icons/document-open.svg")))
+		self.trbtn = QPushButton()
+		self.trbtn.setIcon(QIcon.fromTheme("view-refresh", QIcon("icons/view-refresh.svg")))
+
+		self.completer.setModel(self.tmodel)
+                self.tview.setModel(self.tmodel)
+
+		thlay = QHBoxLayout()
+		thlay.addWidget(self.ted)
+		thlay.addWidget(self.tdbtn)
+		thlay.addWidget(self.trbtn)
 		tvlay = QVBoxLayout()
-		tvlay.addWidget(self.tl)
+		tvlay.addLayout(thlay)
 		tvlay.addWidget(self.tview)
 		self.treew.setLayout(tvlay)
 		self.addTab(self.treew, "Tree")
 
 		self.tview.activated.connect(self.tview_itemActivated)
+		self.tdbtn.clicked.connect(self.change_btn_cb)
+		self.ted.editingFinished.connect(self.ted_editingFinished)
+		self.trbtn.clicked.connect(self.reset_btn_cb)
 
                 self.clear()
+
+	def resizeEvent(self, event):
+		self.tdbtn.setMaximumHeight(self.ted.height())
+		self.trbtn.setMaximumHeight(self.ted.height())
+		QTabWidget.resizeEvent(self, event)
 
 	def le_textChanged(self, text):
 		if (text == ''):
@@ -75,6 +98,31 @@ class FileTree(QTabWidget):
 			if self.tmodel.fileInfo(item).isFile():
 				self.sig_show_file.emit(self.tmodel.fileInfo(item).absoluteFilePath())
 	
+	def ted_editingFinished(self):
+		path = str(self.ted.text())
+		if not os.path.isdir(path):
+			path = os.path.dirname(path)
+		if os.path.isdir(path):
+			self.dir_reset(path)
+		else:
+			self.dir_reset(self.dir_prefix)
+
+	def change_btn_cb(self):
+		fdlg = QFileDialog(None, "Choose directory to browse")
+		fdlg.setFileMode(QFileDialog.Directory)
+		fdlg.setOptions(QFileDialog.ShowDirsOnly) # and QFileDialog.HideNameFilterDetails)
+		fdlg.setDirectory(self.ted.text())
+		if (fdlg.exec_()):
+			browse_dir = fdlg.selectedFiles()[0]
+			self.dir_reset(str(browse_dir))
+
+	def reset_btn_cb(self):
+		self.dir_reset(self.dir_prefix)
+		
+	def dir_reset(self, dirstr):
+		self.tview.setRootIndex(self.tmodel.index(dirstr))
+		self.ted.setText(dirstr)
+
 	def keyPressEvent(self, ev):
 		if ev.key() in [Qt.Key_Up, Qt.Key_Down, Qt.Key_PageUp or Qt.Key_PageDown]:
 			self.lview.keyPressEvent(ev)
@@ -87,11 +135,10 @@ class FileTree(QTabWidget):
 
 	def clear(self):
 		self.is_rel_path = False
-		self.dir_prefix = None 
+		self.dir_prefix = QDir.rootPath() 
 		self.le.clear()
 		self.lview.clear()
-                #self.tview.reset()
-                self.tview.setModel(None)
+                self.dir_reset(self.dir_prefix)
 
 	def add_files(self, flist):
 		self.lview.clear()
@@ -108,10 +155,13 @@ class FileTree(QTabWidget):
 				#self.lview.resizeColumnToContents(1)
 		self.lview.sortByColumn(0, Qt.AscendingOrder)
 
-		header = self.tview.header()
-                self.tview.setModel(self.tmodel)
+		self.hide_view_columns(self.tview)
+
+		self.tview.setRootIndex(self.tmodel.index(self.dir_prefix))
+		self.dir_reset(self.dir_prefix)
+
+	def hide_view_columns(self, view):
+		header = view.header()
 		for col in range(header.count()):
 			if col > 0:
-				self.tview.setColumnHidden(col, True)
-		self.tview.setRootIndex(self.tmodel.index(self.dir_prefix))
-		self.tl.setText("Browsing " + self.dir_prefix)
+				view.setColumnHidden(col, True)
