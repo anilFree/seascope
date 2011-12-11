ed0917d471e12597304803848a34615c0947aebf
Anil Kumar <anil.omkar@gmail.com>
2011-12-10 16:30:25 -0800

    idutils+ctags improvements

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 
 from distutils.core import setup
 
-setup(name='SeaScope',
+setup(name='Seascope',
 	version='0.4a',
 	description='A PyQt GUI front-end for cscope',
 	long_description='A pyQt GUI front-end for cscope. Written in python using pyQt, QScintilla libraries.',
 	url='http://seascope.googlecode.com',
-	packages=['SeaScope',
-		'SeaScope.backend',
-		'SeaScope.backend.plugins',
-		'SeaScope.backend.plugins.cscope',
-		'SeaScope.backend.plugins.gtags',
-		'SeaScope.view'
+	packages=['Seascope',
+		'Seascope.backend',
+		'Seascope.backend.plugins',
+		'Seascope.backend.plugins.cscope',
+		'Seascope.backend.plugins.gtags',
+		'Seascope.view'
 		],
-	package_dir={'SeaScope': 'src'},
-	package_data={'SeaScope': ['icons/*.svg','ui/*.ui']},
+	package_dir={'Seascope': 'src'},
+	package_data={'Seascope': ['icons/*.svg','ui/*.ui']},
 	license="BSD License",
 	classifiers=[
           'Development Status :: 4 - Beta',
@@ -32,5 +32,5 @@ setup(name='SeaScope',
           ],
 	options= {'bdist_rpm':{'requires': 'PyQt4,qscintilla-python,cscope,ctags',
 				'group': 'Development Tools',
-				'vendor': 'The SeaScope Team'}}
+				'vendor': 'The Seascope Team'}}
 	)
