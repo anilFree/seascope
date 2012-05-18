e42b8872c4579b665a9c3aa52553d3cd1959bd56
Anil Kumar <anil.omkar@gmail.com>
2012-05-17 21:01:45 -0700

    Report missing ctags error

--- src/view/CtagsManager.py
+++ src/view/CtagsManager.py
@@ -13,15 +13,22 @@ def ct_query(filename):
 	cmd = 'ctags -n -u --fields=+K -f -'
 	args = cmd.split()
 	args.append(filename)
-	proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-	(out_data, err_data) = _eintr_retry_call(proc.communicate)
-	out_data = out_data.split('\n')
+	try:
+		proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+		(out_data, err_data) = _eintr_retry_call(proc.communicate)
+		out_data = out_data.split('\n')
+	except Exception as e:
+		out_data =  [
+				'Failed to run ctags cmd\tignore\t0;\t ',
+				'cmd: %s\tignore\t0;\t ' % ' '.join(args),
+				'error: %s\tignore\t0;\t ' % str(e),
+				'ctags not installed ?\tignore\t0;\t ',
+			]
 	res = []
 	for line in out_data:
 		if (line == ''):
 			break
 		line = line.split('\t')
-		#print '\t%s' % line
 		num = line[2].split(';', 1)[0]
 		line = [line[0], num, line[3]]
 		res.append(line)
