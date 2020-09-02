820df870dc7662fc5e7e340092efd7ef97c3a61d
Wolfram Sang <wsa@kernel.org>
2020-09-02 09:29:11 +0200

    Fix ambiguous shortcut overload
    When pressing Ctrl+Q, I get:
    'QAction::event: Ambiguous shortcut overload: Ctrl+Q'
    
    Remap 'restart' to Ctrl+R

--- src/Seascope.py
+++ src/Seascope.py
@@ -500,7 +500,7 @@ class SeascopeApp(QMainWindow):
 			m_file.addAction('&Save', self.edit_book.save_current_page, 'Ctrl+S')
 		m_file.addAction('&Close', self.file_close_cb, 'Ctrl+W')
 		m_file.addSeparator()
-		m_file.addAction('&Restart', self.file_restart_cb, QKeySequence.Quit)
+		m_file.addAction('&Restart', self.file_restart_cb, 'Ctrl+R')
 		m_file.addAction('&Quit', self.close, 'Ctrl+Q')
 
 		m_edit = menubar.addMenu('&Edit')
